fe15a2ff30a4be86ac2e10f55f7741726eb8156d
frozenfung <frozenfung@gmail.com>
2014-10-22 09:27:32 +0800

    finish inheritance/ex2

--- inheritance/ex2.rb
+++ inheritance/ex2.rb
@@ -1,7 +1,17 @@
 class Vehicle
-	
+
+	@@num_of_v = 0
+
+	def initialize
+		@@num_of_v ++
+	end
+
 	def self.gas_mileage(g, m)
 		puts "#{m/g} miles per gallon of gas"
+	end
+
+	def self.get_num_of_v
+		puts "there are total #{@@num_of_v} vehicle created"
 	end
 
 end
@@ -13,6 +23,7 @@ end
 
 class MyCar < Vehicle
 	WEIGHT = 180
+	
 end
 
 
